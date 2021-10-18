8869a467bc40dafe2d9327295770aca39047c9be
Pablo Padilla <pgpb.padilla@gmail.com>
2021-10-18 16:08:42 +0200

    chg: Disable MobileOrg configuration
    From now own, only setup MobileOrg integration for
    selected items, e.g. only things that I absolutely
    need access to in my mobile.

--- emacs/init.el
+++ emacs/init.el
@@ -123,12 +123,14 @@
 ;; (setq org-agenda-files (list "~/org"))
 (setq org-agenda-span 'day)
 
+;; MobileOrg options
+;; https://mobileorg.github.io/documentation/#using-dropbox
 ;; Set to the location of your Org files on your local system
-(setq org-directory "~/org")
+;; (setq org-directory "~/org")
 ;; Set to the name of the file where new notes will be stored
-(setq org-mobile-inbox-for-pull "~/org/flagged.org")
+;; (setq org-mobile-inbox-for-pull "~/org/flagged.org")
 ;; Set to <your Dropbox root directory>/MobileOrg.
-(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+;; (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 
 ;; TODO State Configuration
 
