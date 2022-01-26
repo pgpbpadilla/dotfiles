5013928371977c6eb496769faa43d1ab599d09af
Pablo Padilla <pgpb.padilla@gmail.com>
2022-01-26 19:09:32 +0100

    new: use fancy bullets

--- emacs/org.el
+++ emacs/org.el
@@ -82,3 +82,10 @@
         (shell . t)
     )
 )
+
+;; Show fancy bullets
+;; https://github.com/sabof/org-bullets
+(unless (package-installed-p 'org-bullets)
+  (package-install 'org-bullets))
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
