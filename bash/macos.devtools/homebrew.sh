1420a8b47e0e941374ae18628a57352233aaa40e
Pablo Gerardo Padilla Beltran <pgpb.padilla@gmail.com>
2022-03-18 18:58:16 +0100

    fix: brew: remove Homebrew warning, missing sbin dir

--- bash/macos.devtools/homebrew.sh
+++ bash/macos.devtools/homebrew.sh
@@ -3,3 +3,8 @@
 # Enable bash-completion
 # brew install bash-completions
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+
+# Fixes warning:
+# >> Homebrew's "sbin" was not found in your PATH but you have installed
+# >> formulae that put executables in /usr/local/sbin.
+export PATH="/usr/local/sbin:$PATH"
