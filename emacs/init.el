5ae77499bcf36d39d82f451e28b840a663b04f36
Pablo Padilla <pgpb.padilla@gmail.com>
2022-03-18 17:35:15 +0100

    fix: emacs: indentation

--- emacs/init.el
+++ emacs/init.el
@@ -11,9 +11,9 @@
 ;;; load-path
 ;;
 (dolist (i '(
-	           "~/.emacs.d/themes/"
+	     "~/.emacs.d/themes/"
              "~/dotfiles/emacs/"
-	           ))
+	     ))
   ;; Add all at end of `load-path' to avoid conflicts.
   (add-to-list 'load-path (file-name-as-directory i) t))
 
