54ed89f8c09fdeec965c175f643525ae663f828a
Pablo Padilla <pgpb.padilla@gmail.com>
2022-03-30 11:55:18 +0200

    new: bash: Customizations for Linux WSL2

---
+++ bash/common.devtools/golang.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Add Golang binaries to path
+# At the moment it seems it's only:
+# - lazydocker
+# to list all binaries:
+# $ tree ~/go/bin
+export PATH="~/go/bin:${PATH}"
---
+++ bash/common.devtools/terraform.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+complete -C /usr/bin/terraform terraform
