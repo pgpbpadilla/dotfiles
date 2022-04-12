bd061a9c55db6b32a654aa95d8dc083184dca140
Pablo Padilla <pgpb.padilla@gmail.com>
2022-04-12 22:50:25 +0200

    new: emacs: Add abbreviations

--- emacs/abbrev_defs.el
+++ emacs/abbrev_defs.el
@@ -1,13 +1,15 @@
 ;;-*-coding: utf-8;-*-
 (define-abbrev-table 'global-abbrev-table
   '(
-    ("Abbrev" "Abbreviations" nil :count 6)
-    ("abbrev" "abbreviation" nil :count 1)
+    ("Abbrev" "Abbreviations" nil :count 8)
+    ("abbrev" "abbreviation" nil :count 5)
     ("abbrevs" "abbreviations" nil :count 3)
+    ("ae" "ä" nil :count 3)
     ("ema" "Emacs" nil :count 6)
     ("im" "I'm" nil :count 3)
     ("lett" "let the" nil :count 2)
     ("nogo" "not going to" nil :count 2)
+    ("ntt" "new: task tracking" nil :count 1)
     ("td" "Today" nil :count 2)
     ("tfo" "to find out" nil :count 1)
     ("wtft" "was the first time" nil :count 2)
