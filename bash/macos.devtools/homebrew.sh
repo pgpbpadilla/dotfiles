4cd87287d6dbf81aaecac0fccef3be5f2b76b0f8
Padilla Beltran <pablo.padillabeltran@dexxxagl9x6kgy9v.ger.win.int.kn>
2022-08-05 22:05:25 +0200

    fix: homebrew: make brew available in the PATH

--- bash/macos.devtools/homebrew.sh
+++ bash/macos.devtools/homebrew.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# 
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # Enable bash-completion
 # brew install bash-completions
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
@@ -8,3 +11,6 @@
 # >> Homebrew's "sbin" was not found in your PATH but you have installed
 # >> formulae that put executables in /usr/local/sbin.
 export PATH="/usr/local/sbin:$PATH"
+
+
+
