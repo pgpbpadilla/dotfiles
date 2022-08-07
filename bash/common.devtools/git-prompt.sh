ba7129f20919c0d0513434c6b93a87878dd33040
Padilla Beltran <pablo.padillabeltran@dexxxagl9x6kgy9v.ger.win.int.kn>
2022-08-07 18:39:25 +0200

    fix: bash: load bash git prompt for homebrew installations

--- bash/common.devtools/git-prompt.sh
+++ bash/common.devtools/git-prompt.sh
@@ -2,6 +2,14 @@
 
 ## Git prompt
 # https://github.com/magicmonty/bash-git-prompt
+
+# When installed with Homebrew
+if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+  __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
+  GIT_PROMPT_ONLY_IN_REPO=1
+  source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+fi
+
 # *WARN*: The following snippet only becomes os-agnostic if 
 # bash-git-prompt is installed via =git clone=
 if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
