04076a9906d732ebeec0f954a1a8b7357e3b9e4c
Pablo Padilla <pgpb.padilla@gmail.com>
2023-05-07 22:53:51 +0200

    chg: emacs: org: skip RET on zsa-start function
    I'm constantly adding newlines when I use the Start macro within a
    task.

--- emacs/zsa.el
+++ emacs/zsa.el
@@ -1,7 +1,13 @@
 (defun zsa-start ()
   "Focus on task and clock"
   (interactive)
-  (funcall (lookup-key (current-local-map) (kbd "RET")))
+
+  ;;  Disabled cause I often make the mistake of trying
+  ;;  to start while I'm already within the body of the task.
+  ;;  However this function was originally thought to be used
+  ;;  in the context of the Agenda view or while searching for
+  ;;  a heading with C-c h
+  ;;  (funcall (lookup-key (current-local-map) (kbd "RET")))
   (org-narrow-to-subtree)
   (delete-other-windows)
   (org-clock-in)
