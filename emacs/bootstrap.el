607892242aeceb70afbcb3d2f9d53e1479f64b3d
Pablo Padilla <pgpb.padilla@gmail.com>
2023-11-11 23:11:31 +0100

    chg: emacs: parameterize directory to use for org-agenda-files

--- emacs/bootstrap.el
+++ emacs/bootstrap.el
@@ -1,6 +1,6 @@
 ;;; Bootstrap Emacs configuration
 
 ;; set directory to use for loading agenda files
-(defvar org-agenda-dir "~/org/")
+(defvar org-agenda-dir "~/org/31e520d4df")
 
 (load "~/dotfiles/emacs/init.el")
