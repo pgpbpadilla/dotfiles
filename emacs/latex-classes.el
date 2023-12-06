2766338b7bad643d1696b7e833150091a84acec1
Pablo Padilla <pgpb.padilla@gmail.com>
2023-12-06 20:30:24 +0100

    fix: emacs: Set org-latex-classes after Org has loaded

--- emacs/latex-classes.el
+++ emacs/latex-classes.el
@@ -1,5 +1,13 @@
-(add-to-list 'org-latex-classes
-             '("letter" "\\documentclass{letter}"))
+;; https://superuser.com/questions/896741/how-do-i-configure-org-latex-classes-in-emacs
+;; quote:
+;; >> This adjusts the variable as soon as the ox-latex package is loaded.
+;;
+;; =org-latex-classes= is nil on Emacs startup before Org is loaded,
+;; hence we need to wait until Org is loaded so that this variable has a non-nil value.
+(with-eval-after-load 'ox-latex
+  (add-to-list 'org-latex-classes
+             '("letter" "\\documentclass{letter}")))
+
 
 
 ;;; Export as an Emacs package
