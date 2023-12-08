fb8ff9e69d7a963c45c5faa0b4a357243bd01dae
Pablo Padilla <pablo.padillabeltran@kuehne-nagel.com>
2023-12-08 13:44:32 +0100

    fix: gpg: use stable path to update PATH
    When using the path in =brew list gnupg@2.2=
    the path includes the minor version which makes this break on minor
    updates, e.g.
    
    Output of =brew list=:
    
    >> /opt/homebrew/Cellar/gnupg@2.2/2.2.42/bin/gpg
    
    Running =brew info gnupg@2.2= gives the stable path:
    
    >> If you need to have gnupg@2.2 first in your PATH, run:
    >>  echo 'export PATH="/opt/homebrew/opt/gnupg@2.2/bin:$PATH"' ...

--- bash/macos.devtools/gpg.sh
+++ bash/macos.devtools/gpg.sh
@@ -2,4 +2,4 @@
 
 # pin gnupg: 2.4+ have a bug: https://stackoverflow.com/q/76388376/400544
 # fix: https://stackoverflow.com/a/76404609/400544
-export PATH="$(brew --prefix)/Cellar/gnupg@2.2/2.2.41/bin:$PATH"
+export PATH="$(brew --prefix)/opt/gnupg@2.2/bin:$PATH"
