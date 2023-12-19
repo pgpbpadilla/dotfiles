5be5bd69fc52571dc74419c08764e385c52e6e3a
Pablo Padilla <pgpb.padilla@gmail.com>
2023-12-19 21:54:20 +0100

    fix: dev: disable bind keywords, use file-local variables instead
    https://emacs.stackexchange.com/a/76549/11978

--- emacs/init-org.el
+++ emacs/init-org.el
@@ -236,12 +236,6 @@
 ;;; Additional places to use for indexing Org IDs
 (setq org-id-extra-files (my/org-extra-files))
 
-;;; Needed to stop the Org Markdown exporter from
-;; promoting level 2 headings to level 1.
-;; https://emacs.stackexchange.com/a/76549/11978
-;; TODO: This is not secure, should use local variables:
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Specifying-File-Variables.html
-(setq org-export-allow-bind-keywords t)
 
 ;;; Export as an Emacs package
 (provide 'init-org)
