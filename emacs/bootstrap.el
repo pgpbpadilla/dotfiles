fd7c35190313369d637e8bce80a0e4b6477ae7c1
Pablo Padilla <pgpb.padilla@gmail.com>
2024-01-06 22:24:49 +0100

    chg: dev: migrate bootstrap and pgpb to Org + literate programming

--- emacs/bootstrap.el
+++ emacs/bootstrap.el
@@ -1,3 +1,7 @@
-;;; Bootstrap Emacs configuration
+(defvar pgpb-gpg-keys (list "pgpb.padilla@gmail.com"))
+(defvar pgpb-org-dirs (list
+                       "~/org/31e520d4df"
+                       "~/org/0f6de25076"
+                       "~/org/2204c36fc7"))
 
 (load "~/dotfiles/emacs/init.el")
