fbc6c3573d2b6b369eb05cffa5afe389a4008f54
Pablo Padilla <pgpb.padilla@gmail.com>
2024-01-06 23:00:38 +0100

    chg: dev: emacs: modularize pgpb.el
    Extract similar blocks to functions so that I can document each one
    separately

--- emacs/bootstrap.el
+++ emacs/bootstrap.el
@@ -1,7 +1,8 @@
 (defvar pgpb-gpg-keys (list "pgpb.padilla@gmail.com"))
 (defvar pgpb-org-dirs (list
-                       "~/org/31e520d4df"
-                       "~/org/0f6de25076"
-                       "~/org/2204c36fc7"))
+                       "~/org/31e520d4df" ;; agenda files
+                       "~/org/0f6de25076" ;; journal files
+                       "~/org/2204c36fc7" ;; archive files
+                       ))
 
 (load "~/dotfiles/emacs/init.el")
