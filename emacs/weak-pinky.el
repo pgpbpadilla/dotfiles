d15a34c97608b08e3c3bf93a987e02daa1e15dbb
Pablo Padilla <pgpb.padilla@gmail.com>
2024-01-08 21:43:50 +0100

    chg: dev: emacs: keymap: use index+middle finger section motion

--- emacs/weak-pinky.el
+++ emacs/weak-pinky.el
@@ -1,4 +1,11 @@
 (keymap-global-set "M-J" 'next-line)
 (keymap-global-set "M-I" 'previous-line)
 
+(add-hook 'magit-status-mode
+          (lambda ()
+            (keymap-set magit-mode-map "M-e"
+                        'magit-section-backward)
+            (keymap-set magit-mode-map "M-f"
+                        'magit-section-forward)))
+
 (provide 'weak-pinky)
