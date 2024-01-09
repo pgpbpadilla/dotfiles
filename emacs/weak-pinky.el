81979db49c8ab1518a7eaf8571ed4e148e149d1b
Pablo Padilla <pgpb.padilla@gmail.com>
2024-01-09 21:17:57 +0100

    chg: dev: emacs: magit motion: reduce load on pinky finger

--- emacs/weak-pinky.el
+++ emacs/weak-pinky.el
@@ -8,11 +8,12 @@
             (keymap-set magit-mode-map "M-f"
                         'magit-section-forward)))
 
-(add-hook 'magit-status-mode
-          (lambda ()
-            (keymap-set magit-mode-map "M-e"
-                        'magit-section-backward)
-            (keymap-set magit-mode-map "M-f"
-                        'magit-section-forward)))
+(use-package magit
+  :ensure t
+  :bind (:map magit-mode-map
+              ("C-e" . magit-section-backward)
+              ("C-f" . magit-section-forward)
+              ("M-e" . magit-section-backward-sibling)
+              ("M-f" . magit-section-forward-sibling)))
 
 (provide 'weak-pinky)
