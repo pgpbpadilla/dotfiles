d6b365a98a13ceae3608cb462e8e511ea4efe69a
Pablo Padilla <pgpb.padilla@gmail.com>
2024-01-15 21:05:39 +0100

    chg: dev: emacs: replace bootstrap.el with early-init.org

--- emacs/bootstrap.el
+++
@@ -1,8 +0,0 @@
-(defvar pgpb-gpg-keys (list "pgpb.padilla@gmail.com"))
-(defvar pgpb-org-dirs (list
-                       "~/org/31e520d4df" ;; agenda files
-                       "~/org/0f6de25076" ;; journal files
-                       "~/org/2204c36fc7" ;; archive files
-                       ))
-
-(load "~/dotfiles/emacs/init.el")
