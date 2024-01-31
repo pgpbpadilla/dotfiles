ad15ca22cc44f8b37d099fade3faed264cfea793
Pablo Padilla <pgpb.padilla@gmail.com>
2024-01-31 21:58:57 +0100

    chg: dev: emacs: colors for diff in magit

--- emacs/pgpb/pgpb-helpers.el
+++ emacs/pgpb/pgpb-helpers.el
@@ -17,8 +17,8 @@
    ;; Fix magit word coloring, I need more contrast in the changed words
    ;; Changing the the background color to make the diffs easier to read
    ;; search: magit-diff-color
-   '(diff-refine-added ((t (:inherit diff-refine-changed :background "Yellow"))))
-   '(diff-refine-removed ((t (:inherit diff-refine-changed :background "Yellow")))))
+   '(diff-refine-added ((t (:inherit diff-refine-changed :background "yellow"))))
+   '(diff-refine-removed ((t (:inherit diff-refine-changed :background "yellow")))))
 
   (setq 
    ansi-color-faces-vector '[default default default italic underline success warning error]
